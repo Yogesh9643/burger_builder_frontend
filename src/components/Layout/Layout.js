8acbd5b86b09d470ef6ad5afe633d87a7e41ca27
Yogesh9643 <rajputyogesh563@gmail.com>
2020-11-13 13:20:44 +0530

    changes in layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import styles from './Layout.css';
 
 const layout=(props)=>{
-
+    return(
+<Aux>
+    <div>Toolbar, SiderDrawer, Backdrop</div>
+    <main className={styles.Content}>
+        {props.children}
+    </main>
+</Aux>
+    )
 }
 
 export default layout;
