c38e2fcc0fc8feb08d79f81c64e004f268591a9a
Yogesh9643 <rajputyogesh563@gmail.com>
2020-11-13 20:29:53 +0530

    added ingredients js file

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
-import styles from './Layout.css';
+import classes from './Layout.module.css';
 
 const layout=(props)=>{
     return(
 <Aux>
     <div>Toolbar, SiderDrawer, Backdrop</div>
-    <main className={styles.Content}>
+    <main className={classes.Content}>
         {props.children}
     </main>
 </Aux>
